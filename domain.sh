85064b87a679139bcc4038e48c00d9621f795d8c
Pankaj Kargirwar <kargirwar@gmail.com>
2022-03-29 15:44:38 +0530

    Added domain script

---
+++ domain.sh
@@ -0,0 +1,1 @@
+doctl compute domain records create prosql.io --record-name p0 --record-ttl 600 --record-type A --record-data 68.183.92.156
---
+++ firewall.sh
@@ -0,0 +1,2 @@
+doctl compute firewall create --name default-firewall-1 --inbound-rules "protocol:tcp,ports:22,sources:any"
+#doctl compute firewall add-droplets 83b05002-7e29-45fa-a746-5837ca1bde9e --droplet-ids 292848721
